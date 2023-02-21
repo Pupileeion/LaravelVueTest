f7067b445e9e155feee1078408361f488eb4beff
truthanytime <truthteller1995ghost@gmail.com>
2023-02-21 09:52:20 +0100

    first

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/api', function () {
+
+    return response()->json(['title' => 'Hi', 'content' => 'Hello World!'])
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', '*')
+        ->header('Access-Control-Allow-Credentials', true)
+        ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
+        ->header('Accept', 'application/json');
+});
+
+Route::get('/', function () {
+
+    return view('vue')
+    ->with('post', file_get_contents(resource_path('views/dist/index.html')));
+});
